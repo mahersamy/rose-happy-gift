78ce7f9b6dee00c753184cfab987f67f251f1e4b
maher <mahersamy699@yahoo.com>
2025-05-16 04:04:19 +0300

    chore(structure): setup initial folder structure with .gitkeep

--- apps/rose-ecommerce/src/app/app.config.ts
+++ apps/rose-ecommerce/src/app/app.config.ts
@@ -5,11 +5,24 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(withFetch()),
+    providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                  darkModeSelector: '.my-app-dark'
+              }
+            }
+        })
   ],
 };
