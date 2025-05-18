b511c4ed389cb64d2912aa1aa1cd1ec0c5a4b767
maher <mahersamy699@yahoo.com>
2025-05-18 20:38:57 +0300

    feat(home/special-grid): -create spicel grid and responsive

---
+++ apps/rose-ecommerce/src/app/features/pages/home/components/grid-carousel/grid-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SpecialSliderComponent } from "../special-slider/special-slider.component";
+
+@Component({
+  selector: 'app-grid-carousel',
+  imports: [SpecialSliderComponent],
+  templateUrl: './grid-carousel.component.html',
+  styleUrl: './grid-carousel.component.scss'
+})
+export class GridCarouselComponent {
+
+}
--- apps/rose-ecommerce/src/app/features/pages/home/home.component.ts
+++ apps/rose-ecommerce/src/app/features/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SpecialSliderComponent } from "./components/special-slider/special-slider.component";
+import { GridCarouselComponent } from "./components/grid-carousel/grid-carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [SpecialSliderComponent],
+  imports: [SpecialSliderComponent, GridCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
