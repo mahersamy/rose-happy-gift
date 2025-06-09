9f52d70ce77176a84b5b75783360f5b184d65af2
unknown <ahmed@gmail.com>
2025-06-09 19:13:48 +0300

    darkModeFeature

--- apps/rose-ecommerce/src/app/shared/layouts/navbar/navbar.component.ts
+++ apps/rose-ecommerce/src/app/shared/layouts/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MobileViweLinksComponent } from './components/mobile-viwe-links/mobile-viwe-links.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -15,11 +15,20 @@ import { TranslationService } from '../../../core/services/translation.service';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   private readonly _translationService = inject(TranslationService);
+  isDark = false;
   isLogin: boolean = false;
   showLinks: boolean = false;
   isEn: boolean = this._translationService.defaultLang() == 'en' ? true : false;
+
+ ngOnInit(): void {
+     const savedMode = localStorage.getItem('theme');
+    if (savedMode === 'dark') {
+      this.isDark = true;
+      document.documentElement.classList.add('my-app-dark');
+    }
+  }
 
   switchHidden() {
     this.showLinks = !this.showLinks;
@@ -32,5 +41,18 @@ export class NavbarComponent {
       this._translationService.changeLang('en');
     }
     this.isEn = !this.isEn;
+  }
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      document.documentElement.classList.add('my-app-dark');
+            localStorage.setItem('theme', 'dark');
+
+    } else {
+      document.documentElement.classList.remove('my-app-dark');
+            localStorage.setItem('theme', 'light');
+
+    }
   }
 }
